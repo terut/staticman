4c30ac3c23dc7f69c6ceb64df6defefe771df9fb
terut <terut.dev+github@gmail.com>
2013-04-23 19:46:08 +0900

    Add load file

--- lib/staticman.rb
+++ lib/staticman.rb
@@ -1,5 +1,27 @@
+# Load Rails/Railtie
+begin
+  require 'rails'
+rescue LoadError
+
+end
+
+$stderr.puts <<-EOC if !defined?(Rails)
+warning: no framework detected.
+
+Your Gemfile might not be configured properly.
+----- e.g. -----
+Rails:
+  gem 'staticman'
+
+EOC
+
 require "staticman/version"
+require 'staticman/configurable'
+
+if defined?(Rails)
+  require 'staticman/railtie'
+end
 
 module Staticman
-  # Your code goes here...
+  include Configurable
 end
